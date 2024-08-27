e8388016efe945bdce9b48bf8b316a7b98dd0e24
Gianni <giannirosato@proton.me>
2024-08-27 02:10:26 -0400

    feat: psy build script + fixed svt defaults

--- cmake/Modules/LocalAom.cmake
+++ cmake/Modules/LocalAom.cmake
@@ -102,7 +102,7 @@ else()
         set(CONFIG_ML_PART_SPLIT 0 CACHE INTERNAL "")
     else()
         FetchContent_Declare(
-            libaom URL "https://aomedia.googlesource.com/aom/+archive/${AVIF_LOCAL_AOM_GIT_TAG}.tar.gz" BINARY_DIR
+            libaom URL "https://gitlab.com/damian101/aom-psy101/+archive/${AVIF_LOCAL_AOM_GIT_TAG}.tar.gz" BINARY_DIR
                        "${AOM_BINARY_DIR}" UPDATE_COMMAND ""
         )
     endif()
